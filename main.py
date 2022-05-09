f06e9b4a477dce7b88e2ba80ef2e6f0eb7ae647a
Shapi <63413434+Harrpy@users.noreply.github.com>
2022-05-09 03:36:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 import random
-#^the header ....hehee head , well anyway shapi u had this in class pretend u know whats going on, will ya?
 
 #de beginnin
-user_input = input("Hey babe, how are u feelin'? :) <3\n" )
+user_input = input("Hey, how are u feelin'? :) <3\n" )
 #LIST en here .. hehehhee
 good_list = ["Good, hbu?", "Good u?", "good, hbu?", "good hbu?", "good hbu","Good, u?","Good, u", "good u","good u?", "Good, and you?", "Good, wbu?","good wbu?"]
 
 ok_list = ["okay", "OK", "oki", "okay", "meh", "Meh"]
 
-bad_list = ["fucking shit m8", "fucking shit", "foken shit", "foken shit m8", "shit", "shite",  "foken shite", "bad", "bad, hbu?"]
+bad_list = ["bad", "bad, hbu?"]
 
 k_list = ["K", "k", "Good", "good","Gud","gud", "Pretty good", "pretty good"]
 
 dodge_list = ["I don't wanna talk about it, i don't wanna talk about it", "i dont wanna talk about it", "I don't feel like talking about it", "i don't feel like talking about it", "i dont feel like talking about"]
 
-
-
 #de answers
 
 if user_input in good_list:
-  sayings = ["That is nice to hear, I feel amazing angel!","amazing now that you are home my angel, want your honeytub slurrrrped up real good bbygurl?","niceuuuu <3 uwu\nme too, did I already tell you how much I love you????","pog, me too","poggers babe, so do I"]
+  sayings = ["That is nice to hear, I feel amazing angel!","amazing now that you are home my angel,"niceuuuu <3 uwu\nme too, did I already tell you how much I love you????","pog, me too","poggers babe, so do I"]
   print(random.choice(list(sayings)))
 
 if user_input in bad_list:
-  briish = ["ight m8", "kekw", "kekW", "u wanna get shanked m8?", "ye doen fok all round ere but foken nag innit?", "feelsbad", "feelsbadman", "rip", "rip" "lol", "F", "f", "f in chat", "sucks to be you"]
+  briish = ["ight m8", "kekw", "kekW", "u wanna get shanked m8?", "feelsbad", "feelsbadman", "rip", "rip" "lol", "F", "f", "f in chat", "sucks to be you"]
   print(random.choice(list(briish)))
 
 if user_input in ok_list:
@@ -32,26 +29,10 @@ if user_input in ok_list:
 if user_input in dodge_list:
   print("I respecc dah")
 
-
-if user_input in k_list:
-  print("ight how about you go fuck yourself then hmmm? fucking bitch, get outta me country until ye learn how to live with tegridehhh yeeeeeeah brother trump 2026")
-  
 if user_input in ["'Murica","Murica","'murica","murica" "kkona brother"]:
-  print("fuck yeah! TRUMP BABY")
+  print("god bless america")
 
 if user_input in ["China", "china"]:
     print("https://tenor.com/view/trump-trump-face-gif-12025846")
 
   #MISC#s
-if user_input =="what defeated the nazis?":
-  print("idk, jew jitsu???")
-
-if user_input =="dababy":
-  print("kill dababy!")
-
-if user_input == "a little sussy":
-  print("amogus boner")
-
-# if user_input != good_list or bad_list or k_list or ok_list or dodge_list:
-#   print("english motherfucker, do you speak it?")
-
